fd9ce29e18f0979105405365e18032274e19da47
RaisanJMR <raisanjmr@protonmail.com>
2021-05-01 12:05:49 +0530

    first commit

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import logo from '../../img/logo.png'
 
+
+
 const Header = () => {
-  return (
-    <header className='center'>
-      <img src={logo} alt='' />
-    </header>
-  )
+    return (
+        <header className="center">
+            <img src={logo} alt="logo"/>
+        </header>
+    )
 }
 
-export default Header
+export default Header;
